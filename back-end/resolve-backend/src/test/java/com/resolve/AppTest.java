0f50a86f5a4eaa2587a9b2afa529d74bf6b0dc10
kavidoi <kdoi@email.com>
2025-05-01 20:28:02 -0400

    refactor: update test to use JUnit 5

--- back-end/resolve-backend/src/test/java/com/resolve/AppTest.java
+++ back-end/resolve-backend/src/test/java/com/resolve/AppTest.java
@@ -1,31 +1,19 @@
 package com.resolve;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest {
+    
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * A simple test to verify the application starts.
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testAppStarts() {
+        assertTrue(true, "Application should start successfully");
     }
 
     /**
